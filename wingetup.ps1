b4ece8041b7472e2ebe38332719e4a1f344adabf
hydra-win@3os.re <hydra-win@3os.re>
2023-07-05 12:35:03 +0300

    20230705.1235

--- wingetup.ps1
+++ wingetup.ps1
@@ -1,109 +1,113 @@
-# PowerShell Script
+<#
+.SYNOPSIS
+    A script that updates Winget packages, exports the configuration, and pushes to a Git repository.
+#>
+
 $ErrorActionPreference = 'Continue'
 
-# Check if the execution policy permits running scripts
-$currentPolicy = Get-ExecutionPolicy -Scope CurrentUser
-
-if ($currentPolicy -ne 'RemoteSigned') {
-    # Prompt the user
-    $message = "The current execution policy is set to '$currentPolicy'. This script requires the execution policy to be set to 'RemoteSigned'. Changing the execution policy might have security implications. For more information, see about_Execution_Policies at https://go.microsoft.com/fwlink/?LinkID=135170. Do you want to proceed?"
-    $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Sets the execution policy to 'RemoteSigned'."
-    $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", "Exits the script."
-    $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
-    $result = $host.ui.PromptForChoice("Change Execution Policy", $message, $options, 1)
-    
-    # Check user selection
-    switch ($result) {
-        0 { # User selected Yes
-            try {
-                Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
-            } catch {
-                Write-Host "Failed to set execution policy. Please run the script as an administrator or change the execution policy manually."
+function Set-ExecutionPolicyIfRequired {
+    $currentPolicy = Get-ExecutionPolicy -Scope CurrentUser
+    if ($currentPolicy -ne 'RemoteSigned') {
+        # Prompt the user
+        $message = "The current execution policy is set to '$currentPolicy'. This script requires the execution policy to be set to 'RemoteSigned'. Changing the execution policy might have security implications. Do you want to proceed?"
+        $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Sets the execution policy to 'RemoteSigned'."
+        $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", "Exits the script."
+        $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
+        $result = $host.ui.PromptForChoice("Change Execution Policy", $message, $options, 1)
+        
+        switch ($result) {
+            0 { # User selected Yes
+                try {
+                    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+                } catch {
+                    Write-Host "Failed to set execution policy. Please run the script as an administrator or change the execution policy manually."
+                    exit 1
+                }
+            }
+            1 { # User selected No
+                Write-Host "User chose not to change the execution policy. Exiting script."
                 exit 1
             }
         }
-        1 { # User selected No
-            Write-Host "User chose not to change the execution policy. Exiting script."
-            exit 1
-        }
     }
 }
 
-# Profile path for current user and all hosts
-$profilePath = $PROFILE.CurrentUserAllHosts
-
-# Check if the profile file exists, create it if not
-if (!(Test-Path $profilePath)) {
-    New-Item -Type File -Path $profilePath -Force
-}
-
-# Check if the alias exists
-$existingAlias = Get-Alias -Name "wingetup" -ErrorAction SilentlyContinue
-$scriptPath = "`"$((Get-Item $MyInvocation.MyCommand.Path).FullName)`""
-
-# Update or create the alias
-if ($existingAlias) {
-    $existingAliasPath = $existingAlias.Definition
-    if ($existingAliasPath -ne $scriptPath) {
-        # Update the alias if the script path has changed
-        "Set-Alias -Name wingetup -Value $scriptPath" | Out-File -Encoding utf8 -FilePath $profilePath
+function UpdateOrCreateAlias {
+    $profilePath = $PROFILE.CurrentUserAllHosts
+    if (!(Test-Path $profilePath)) {
+        New-Item -Type File -Path $profilePath -Force
+    }
+    
+    $scriptPath = "`"$PSScriptRoot\$($MyInvocation.MyCommand.Name)`""
+    $aliasContent = "Set-Alias -Name wingetup -Value $scriptPath"
+    
+    # Check if the alias content already exists in the profile
+    $profileContent = Get-Content $profilePath -Raw
+    if ($profileContent -notmatch [regex]::Escape($aliasContent)) {
+        $aliasContent | Out-File -Append -Encoding utf8 -FilePath $profilePath
         Write-Host "Alias 'wingetup' has been updated."
-        # Reload the profile
-        . $profilePath
+        . $profilePath # Reload the profile
     }
-} else {
-    # Create the alias if it doesn't exist
-    "Set-Alias -Name wingetup -Value $scriptPath" | Out-File -Append -Encoding utf8 -FilePath $profilePath
-    Write-Host "Alias 'wingetup' has been created."
-    # Reload the profile
-    . $profilePath
 }
 
-# Define function for writing colored text
+function InstallGitIfNotExists {
+    try {
+        git --version | Out-Null
+    } catch {
+        Write-Host "Installing Git via winget..."
+        winget install Git.Git
+    }
+}
+
 function Write-Color($Text, $Color) {
     Write-Host $Text -ForegroundColor $Color -NoNewline
     Write-Host
 }
 
-# Check if Git is installed, if not install it
-try {
-    git --version | Out-Null
-}
-catch {
-    Write-Host "Installing Git via winget..."
-    winget install Git.Git
-}
+function Main {
+    Set-ExecutionPolicyIfRequired
+    UpdateOrCreateAlias
+    InstallGitIfNotExists
+    
+    # Set working directory as the script location
+    $scriptDir = $PSScriptRoot
+    Set-Location $scriptDir
+    
+    # Update the Git repository
+    Write-Host "Updating Git repository..."
+    git pull
+    
+    # Winget packages update
+    Write-Color "==> Running Winget Updates..." Yellow
+    winget upgrade --all
+    
+    # Create a JSON dump file with hostname
+    $HostName = [System.Net.Dns]::GetHostName()
+    $jsonFile = "$env:USERPROFILE\$HostName.json"
 
-# Set working directory as the script location
-$scriptDir = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
-Set-Location $scriptDir
+    # Export installed packages to a variable
+    Write-Color "==> Creating JSON dump file..." Yellow
+    $wingetOutput = winget export --output $jsonFile --include-versions 2>&1
 
-# Update the Git repository
-Write-Host "Updating Git repository..."
-git pull
+    # Filter out the unwanted lines and save to JSON file
+    $filteredOutput = $wingetOutput | Where-Object { $_ -notmatch "Installed version of package is not available from any source" }
+    $filteredOutput | Out-File -FilePath $jsonFile
 
-# Winget packages update
-Write-Color "==> Running Winget Updates..." Yellow
-winget upgrade --all
+    # Check if git is available and initialized
+    if (Test-Path .git) {
+        # Add and commit changes to the git repository
+        Write-Color "==> Committing changes to Git repository..." Yellow
+        git add .
+        git commit -m "Update packages on $HostName"
 
-# Create a JSON dump file with hostname
-$HostName = [System.Net.Dns]::GetHostName()
-$wingetFileName = "./WingetExport.${HostName}.json"
-Write-Color "==> Creating JSON dump file..." Yellow
-
-winget export -o $wingetFileName 2>&1 | ForEach-Object {
-    $message = $_.ToString()
-    if (-not $message.StartsWith("Installed package is not available from any source")) {
-        Write-Host $message
+        # Push the changes to the Git repository
+        Write-Color "==> Pushing changes to Git repository..." Yellow
+        git push
+    } else {
+        Write-Color "No Git repository found. Skipping the git push step." Red
     }
+    
+    Write-Color "==> All Updates & Cleanups Finished" Green
 }
 
-
-# Pushing to repo
-$Date = Get-Date -Format 'yyyyMMdd.HHmm'
-Write-Host "Pushing to repository..."
-git add .
-git commit -m "${Date}_update"
-git push
-
-Write-Color "==> All Updates & Cleanups Finished" Green
+Main
