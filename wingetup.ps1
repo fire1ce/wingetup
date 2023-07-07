c8a88e9ad9faf31ca19e09986537ae0aaea2d9e9
hydra-win@3os.re <hydra-win@3os.re>
2023-07-08 01:44:45 +0300

    20230708.0144

--- wingetup.ps1
+++ wingetup.ps1
@@ -89,20 +89,9 @@ function Main {
     $HostName = [System.Net.Dns]::GetHostName()
     $jsonFile = "$env:USERPROFILE\$HostName.json"
 
-    # Export installed packages to a variable
+    # Export installed packages to a file directly
     Write-Color "==> Creating JSON dump file..." Yellow
-    $wingetOutput = winget export --output $jsonFile --include-versions 2>&1
-
-    if ($LASTEXITCODE -ne 0) {
-        Write-Color "The 'winget export' command failed with exit code $LASTEXITCODE" Red
-    }
-
-    Write-Color "Winget output: $wingetOutput" Cyan
-
-    # Filter out the unwanted lines and save to JSON file
-    $filteredOutput = $wingetOutput | Where-Object { $_ -notmatch "Installed version of package is not available from any source" }
-    $filteredOutput | Out-File -FilePath $jsonFile
-
+    winget export --output $jsonFile --include-versions
 
     # Check if git is available and initialized
     if (Test-Path .git) {
